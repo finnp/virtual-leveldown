caf369188497b543a7512cd221142bcb4802e8f0
Finn <derfinn@gmail.com>
2014-11-27 00:20:33 +0100

    Add special tests, fix inmemory

--- index.js
+++ index.js
@@ -3,14 +3,11 @@ var AbstractIterator  = require('abstract-leveldown').AbstractIterator
 var inherits = require('inherits')
 var leveldown = require('leveldown')
 var memdown = require('memdown')
-var inMemory = {}
-
 
 // Iterator
 
 function VirtualIterator(db, options) {
   AbstractIterator.call(this, db)
-  
   
   this.memIter = db._memdown.iterator(options)
   this.levelIter = db._leveldown.iterator(options)
@@ -39,9 +36,8 @@ function VirtualDOWN(location) {
   
   this._leveldown = leveldown(this.location)
   this._memdown = memdown(this.location)
+  this._inMemory = {}
 }
-
-
 
 VirtualDOWN.prototype._open = function (options, callback) {
   this._leveldown.open(options, callback)
@@ -52,28 +48,28 @@ VirtualDOWN.prototype._close = function (options, callback) {
 }
 
 VirtualDOWN.prototype._get = function (key, options, callback) {
-  if(inMemory[key]) {
+  if(this._inMemory[key]) {
     this._memdown.get(key, options, callback)
   } else {
-    inMemory[key] = true
+    this._inMemory[key] = true
     this._leveldown.get(key, options, callback)
   }
 }
 
 VirtualDOWN.prototype._put = function (key, value, options, callback) {
-  inMemory[key] = true
+  this._inMemory[key] = true
   this._memdown.put(key, value, options, callback)
 }
 
 VirtualDOWN.prototype._del = function (key, options, callback) {
-  inMemory[key] = true
+  this._inMemory[key] = true
   this._memdown.del(key, options, callback)
 }
 
 VirtualDOWN.prototype._batch = function (array, options, callback) {
   array.forEach(function (operation) {
-    inMemory[operation.key] = true
-  })
+    this._inMemory[operation.key] = true
+  }.bind(this))
   this._memdown.batch(array, options, callback)
 }
 
--- test.js
+++ test.js
@@ -20,3 +20,129 @@ require('abstract-leveldown/abstract/batch-test').all(VirtualDOWN, test, testCom
 require('abstract-leveldown/abstract/close-test').close(VirtualDOWN, test, testCommon)
 
 require('abstract-leveldown/abstract/iterator-test').all(VirtualDOWN, test, testCommon)
+
+var level = require('leveldown')
+test('working with original copy', function (t) {
+  var location = testCommon.location()
+  
+  var testData = [
+    {type: 'put', key: '01', value: 'cat'},
+    {type: 'put', key: '02', value: 'katze'},
+    {type: 'put', key: '03', value: 'gata'}
+  ]
+  
+  t.test('init orignal db', function (t) {
+    var db = level(location)
+    
+    db.open(function () {
+      db.batch(testData, function() {
+        db.close(function () {
+          t.end()
+        })
+      })
+    })
+  })
+  
+  t.test('get original values', function (t) {
+    var virtualDB = VirtualDOWN(location)
+    virtualDB.open(function () {
+      virtualDB.get('01',function (err, value) {
+        t.notOk(err, 'no error')
+        t.equals(value.toString(), 'cat')
+        virtualDB.close(function () {
+          t.end()
+        })
+      })
+    })  
+  })
+  
+  t.test('put new value', function (t) {
+    var virtualDB = VirtualDOWN(location)
+    
+    virtualDB.open(function () {
+      virtualDB.put('04', 'kissa', function () {
+        virtualDB.get('04', {asBuffer: false}, function (err, value) {
+          t.equals(value, 'kissa', 'added')
+          virtualDB.close(function () {
+            t.end()
+          })
+        })
+      })
+    })
+  })
+  
+  t.test('del value', function (t) {
+    var virtualDB = VirtualDOWN(location)
+    
+    virtualDB.open(function () {
+      virtualDB.del('02', function () {
+        virtualDB.get('02', {asBuffer: false}, function (err, value) {
+          t.ok(err, 'not found')
+          virtualDB.close(function () {
+            t.end()
+          })
+        })
+      })
+    })
+  })
+  
+  t.test('batch del/put', function (t) {
+    var virtualDB = VirtualDOWN(location)
+    
+    var changes = [
+      {type: 'del', key: '03'},
+      {type: 'put', key: '05', value: 'chat'}
+    ]
+    
+    virtualDB.open(function () {
+      virtualDB.batch(changes, function () {
+        virtualDB.get('05', {asBuffer: false}, function (err, value) {
+          t.equals(value, 'chat')
+          virtualDB.close(function () {
+            t.end()
+          })
+        })
+      })
+    })
+  })
+  
+  t.test('iterator', function (t) {
+    var virtualDB = VirtualDOWN(location)
+    
+    var expected = [
+      { key: '01', value: 'cat'},
+      { key: '04', value: 'kissa' },
+      { key: '05', value: 'chat' }
+    ]
+    
+    virtualDB.open(function () {
+      var iterator = virtualDB.iterator()
+      testCommon.collectEntries(iterator, function (err, entries) {
+        t.notOk(err, 'no err')
+        t.equals(expected.length, entries.length, 'correct number of entries')
+        entries.forEach(function (e, index) {
+          t.equals(e.key.toString(), expected[index].key, 'correct key')
+          t.equals(e.value.toString(), expected[index].value, 'correct value')
+        })
+        virtualDB.close(function () {
+          t.end()
+        })
+      })
+    })
+  })
+  
+  t.test('unchanged original', function (t) {
+    var db = level(location)
+    
+    db.open(function () {
+      var iterator = db.iterator()
+      testCommon.collectEntries(iterator, function (err, entries) {
+        entries.forEach(function (e, index) {
+          t.equals(e.key.toString(), testData[index].key, 'correct key')
+          t.equals(e.value.toString(), testData[index].value, 'correct value')
+        })
+        t.end()
+      })
+    })
+  })
+})
